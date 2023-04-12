ac583a49b5ceee6d30620d56a22b33ba0afa0605
JWillisK <129218911+JWillisK@users.noreply.github.com>
2023-04-12 09:56:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+// Set the duration of the slide transition in milliseconds
+const slideDuration = 5000; // 5 seconds
+
+// Select all slides
 const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
 
-function showSlide(index) {
-  slides[currentSlide].classList.remove("active");
-  slides[index].classList.add("active");
-  currentSlide = index;
+// Initialize the slide index
+let slideIndex = 0;
+
+// Start the slideshow
+function startSlideshow() {
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+
+    // Increment the slide index
+    slideIndex++;
+
+    // Reset the slide index if it's greater than the number of slides
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    // Show the current slide
+    slides[slideIndex - 1].classList.add("active");
+
+    // Set a timeout to call the function again
+    setTimeout(startSlideshow, slideDuration);
 }
 
-function nextSlide() {
-  let nextIndex = (currentSlide + 1) % slides.length;
-  showSlide(nextIndex);
-}
-
-setInterval(nextSlide, 2000); // Change images every 2 seconds (2000 ms)
+// Start the slideshow on page load
+window.addEventListener("load", startSlideshow);
